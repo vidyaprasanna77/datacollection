553dd00ab6841b7b9cc3505893ce8c64e4e191a7
vidyaprasanna77 <vidyaravichander@gmail.com>
2022-12-13 11:48:08 +0530

    extraction methods included

--- datascrapping.py
+++ datascrapping.py
@@ -4,3 +4,19 @@ page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 print(html)
+
+# extraction using string method
+title_index = html.find("<title>")
+print(title_index)
+
+# to get the index of title itself
+start_index = title_index + len("<title>")
+print(start_index)
+
+# index of closing tag 
+end_index = html.find("</title>")
+print(end_index)
+
+# extracting the title
+title = html[start_index:end_index]
+print(title)
